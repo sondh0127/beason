0bed7cce0737349fff36f349f62f65de7bd243b9
Son Hong Do <sondh0127@gmail.com>
2022-08-08 16:31:47 +0700

    chore: config root for module

--- packages/nuxt-trpc-query/playground/nuxt.config.ts
+++ packages/nuxt-trpc-query/playground/nuxt.config.ts
@@ -1,11 +1,17 @@
+import { resolve } from 'path'
 import { defineNuxtConfig } from 'nuxt'
 import MyModule from '..'
 
 export default defineNuxtConfig({
+  srcDir: __dirname,
+  rootDir: resolve(__dirname, '..'),
   modules: [
     MyModule,
   ],
   myModule: {
     addPlugin: true,
+  },
+  typescript: {
+    strict: true,
   },
 })
